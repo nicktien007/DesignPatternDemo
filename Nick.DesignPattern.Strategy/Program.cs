7e3b23a1725130d13cca327f5e3aa181fbef973e
田正雨 <proad01@yahoo.com.tw>
2019-03-19 11:45:25 +0800

    Init

---
+++ Nick.DesignPattern.Strategy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nick.DesignPattern.Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
